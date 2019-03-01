6a0f005dadb755bb51867e45ca203128bfdeed04
Patrick McDonnell <patrick.g.mcdonnell@gmail.com>
2019-03-01 10:31:31 +0000

    Add coveralls to project. Coverage currently 8.9%

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule IslandsEngine.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -22,8 +24,8 @@ defmodule IslandsEngine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 end
