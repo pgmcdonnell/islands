3fb7edbc3107ada9279e8727569ad57ad1bfb556
Patrick McDonnell <patrick.g.mcdonnell@gmail.com>
2019-03-04 15:42:03 +0000

    add persistence of state in ets

--- lib/islands_engine/game.ex
+++ lib/islands_engine/game.ex
@@ -15,9 +15,14 @@ defmodule IslandsEngine.Game do
   end
 
   def init(name) do
+    send(self(), {:set_state, name})
+    {:ok, fresh_state(name)}
+  end
+
+  defp fresh_state(name) do
     player1 = %{name: name, board: Board.new(), guesses: Guesses.new()}
     player2 = %{name: nil, board: Board.new(), guesses: Guesses.new()}
-    {:ok, %{player1: player1, player2: player2, rules: %Rules{}}, @timeout}
+    %{player1: player1, player2: player2, rules: %Rules{}}
   end
 
   def via_tuple(name), do: {:via, Registry, {Registry.Game, name}}
@@ -115,9 +120,26 @@ defmodule IslandsEngine.Game do
     end
   end
 
+  def handle_info({:set_state, name}, _state_data) do
+    state_data =
+      case :ets.lookup(:game_state, name) do
+        [] -> fresh_state(name)
+        [{_key, state}] -> state
+      end
+
+    :ets.insert(:game_state, {name, state_data})
+    {:noreply, state_data, @timeout}
+  end
+
   def handle_info(:timeout, state_data) do
     {:stop, {:shutdown, :timeout}, state_data}
   end
+
+  def terminate({:shutdown, :timeout}, state_data) do
+    :ets.delete(:game_state, state_data.player1.name)
+    :ok
+  end
+  def terminate(_reason, _state), do: :ok
 
   defp player_board(state_data, player), do: Map.get(state_data, player).board
 
@@ -140,5 +162,8 @@ defmodule IslandsEngine.Game do
 
   defp update_rules(state_data, rules), do: %{state_data| rules: rules}
 
-  defp reply_success(state_data, reply), do: {:reply, reply, state_data, @timeout}
+  defp reply_success(state_data, reply) do
+    :ets.insert(:game_state, {state_data.player1.name, state_data})
+    {:reply, reply, state_data, @timeout}
+  end
 end
--- test/game_supervisor_test.exs
+++ test/game_supervisor_test.exs
@@ -3,9 +3,18 @@ defmodule GameSupervisorTest do
 
   alias IslandsEngine.{Game, GameSupervisor}
 
+  setup do
+    {:ok, game} = GameSupervisor.start_game("Patrick")
+
+    on_exit fn ->
+      GameSupervisor.stop_game("Patrick")
+    end
+
+    {:ok, %{game: game}}
+  end
+
   describe "supervisor manages game" do
-    test "starts successfully" do
-      {:ok, _game} = GameSupervisor.start_game("Patrick")
+    test "starts successfully", %{game: _game} do
       via = Game.via_tuple("Patrick")
       process = GenServer.whereis(via)
       Game.add_player(via, "Katie")
@@ -15,18 +24,16 @@ defmodule GameSupervisorTest do
       assert :players_set == state_data.rules.state
     end
 
-    test "stops sucessfully" do
-      {:ok, _game} = GameSupervisor.start_game("Danaidh")
-      via = Game.via_tuple("Danaidh")
+    test "stops sucessfully", %{game: _game} do
+      via = Game.via_tuple("Patrick")
       process = GenServer.whereis(via)
-      GameSupervisor.stop_game("Danaidh")
+      GameSupervisor.stop_game("Patrick")
 
       assert false == Process.alive?(process)
     end
 
-    test "handles restarting game processes which crash" do
-      {:ok, game} = GameSupervisor.start_game("Katie")
-      via = Game.via_tuple("Katie")
+    test "handles restarting game processes which crash", %{game: game} do
+      via = Game.via_tuple("Patrick")
 
       initial_process = GenServer.whereis(via)
       Process.exit(game, :kaboom)
@@ -37,7 +44,91 @@ defmodule GameSupervisorTest do
       assert false == Process.alive?(initial_process)
       assert true == Process.alive?(restarted_process)
     end
-
   end
 
+  describe "adding a player" do
+    test "to an initialized game works", %{game: game} do
+      assert :ok == Game.add_player(game, "Wilma")
+    end
+
+    test "fails when we already have two players", %{game: game} do
+      Game.add_player(game, "Wilma")
+      assert :error == Game.add_player(game, "Dino")
+    end
+  end
+
+  describe "positioning an island" do
+    test "works when the island and coordinates are valid", %{game: game} do
+      assert :ok == Game.add_player(game, "Wilma")
+      assert :ok == Game.position_island(game, :player1, :atoll, 1, 1)
+    end
+
+    test "fails when the island coordinates are wrong", %{game: game} do
+      assert :ok == Game.add_player(game, "Wilma")
+      assert {:error, :invalid_coordinate} == Game.position_island(game, :player1, :atoll, 10, 10)
+    end
+
+    test "fails when the island is unknown type", %{game: game} do
+      assert :ok == Game.add_player(game, "Wilma")
+      assert {:error, :invalid_island_type} == Game.position_island(game, :player1, :wrong, 5, 5)
+    end
+
+    test "fails when the game is not in set_islands state", %{game: game} do
+      assert :error == Game.position_island(game, :player1, :atoll, 5, 5)
+    end
+  end
+
+  describe "setting islands" do
+    test "is denied before we add a second player", %{game: game} do
+      assert :error == Game.set_islands(game, :player1)
+    end
+    test "is denied before positioning all islands", %{game: game} do
+      Game.add_player(game, "Wilma")
+      Game.position_island(game, :player1, :atoll, 1, 1)
+      assert {:error, :not_all_islands_positioned} = Game.set_islands(game, :player1)
+    end
+
+    test "works after positioning all islands", %{game: game} do
+      Game.add_player(game, "Wilma")
+      position_all_islands_for_player(game, :player1)
+      assert {:ok, board} = Game.set_islands(game, :player1)
+    end
+  end
+
+  describe "guessing coordinates" do
+    test "works for valid coordinate", %{game: game} do
+      Game.add_player(game, "Wilma")
+      position_all_islands_for_player(game, :player1)
+      position_all_islands_for_player(game, :player2)
+      Game.set_islands(game, :player1)
+      Game.set_islands(game, :player2)
+      assert {:miss, :none, :no_win} = Game.guess_coordinate(game, :player1, 9, 9)
+    end
+
+    test "fails if it isn't players turn", %{game: game} do
+      Game.add_player(game, "Wilma")
+      position_all_islands_for_player(game, :player1)
+      position_all_islands_for_player(game, :player2)
+      Game.set_islands(game, :player1)
+      Game.set_islands(game, :player2)
+      assert :error = Game.guess_coordinate(game, :player2, 9, 9)
+    end
+
+    test "fails if coordinates are invalid", %{game: game} do
+      Game.add_player(game, "Wilma")
+      position_all_islands_for_player(game, :player1)
+      position_all_islands_for_player(game, :player2)
+      Game.set_islands(game, :player1)
+      Game.set_islands(game, :player2)
+      assert {:error, :invalid_coordinate} = Game.guess_coordinate(game, :player1, 12, 19)
+    end
+  end
+
+  defp position_all_islands_for_player(game, player) do
+    Game.position_island(game, player, :atoll, 1, 1)
+    Game.position_island(game, player, :dot, 5, 2)
+    Game.position_island(game, player, :square, 5, 3)
+    Game.position_island(game, player, :s_shape, 8, 1)
+    Game.position_island(game, player, :l_shape, 1, 5)
+  end
 end
